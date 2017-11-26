c3f3082e1bd107af1a910d3089fb694b125ebbcf
cliu5 <cliu5@stuy.edu>
2017-11-26 00:46:49 -0500

    Create ReverseCharSequence.java

---
+++ 04ReverseCharSequence/ReverseCharSequence.java
@@ -0,0 +1,32 @@
+import java.lang.CharSequence;
+public class ReverseCharSequence implements CharSequence{
+    private String stuff;
+    public  ReverseCharSequence(String x){
+	stuff="";
+	for (int i=x.length()-1;i>=0;i--){
+	    stuff+=x.substring(i,i+1);
+	}    
+    }
+    public char charAt(int index){
+	return stuff.charAt(index);
+    }
+    public int length(){
+	return stuff.length();
+    }
+    public CharSequence subSequence(int start, int end){
+	return stuff.substring( start , end);
+    }
+
+
+ public static void main(String[] args){
+	ReverseCharSequence x = new ReverseCharSequence("ABCDEF");
+	System.out.println(x);
+	for (int i = 0; i < x.length(); i++){
+	    System.out.println(x.charAt(i));
+	}
+	System.out.println(x.length());		       
+	System.out.println(x.subSequence(0,4));
+	
+ }
+
+}
