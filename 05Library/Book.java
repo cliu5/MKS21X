b3299eeaa0a51d0edead888a27e7da3d2f24ccf9
cliu5 <cliu5@stuy.edu>
2017-12-03 15:14:03 -0500

    Update Book.java

--- 05Library/Book.java
+++ 05Library/Book.java
@@ -1,1 +1,26 @@
-
+public class Book{
+  private String author;
+  private String title;
+  private String ISBN;
+  
+  public Book(){
+  }
+  
+  public Book(String _author, String _title, String _ISBN){
+    author=_author;
+    title=_title;
+    ISBN=_ISBN;
+  }
+  public String getAuthor(){
+    return author;
+  }
+  public String getTitle(){
+    return title;
+  }
+  public String getISBN(){
+    return ISBN
+  }
+  public String toString(){
+    return title+" "+ author + " "+ISBN;
+  }
+}
