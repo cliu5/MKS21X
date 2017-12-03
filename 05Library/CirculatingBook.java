d93901d27d447e51d53d1efc67101a2aa6dc82c4
cliu5 <cliu5@stuy.edu>
2017-12-03 15:39:42 -0500

    Update CirculatingBook.java

--- 05Library/CirculatingBook.java
+++ 05Library/CirculatingBook.java
@@ -1,2 +1,24 @@
 public class CirculatingBook extends LibraryBook{
+  private String currentHolder;
+  private String dueDate;
+  public CirculatingBook(String _author, String _title, String _ISBN, String _callNumber){
+    super(_author,_title,_ISBN,_callNumber);
+    currentHolder=null;
+    dueDate=null;
+  }
+  public void checkout(String patron, String _dueDate){
+    currentHolder=patron;
+    dueDate=_dueDate;
+  }
+  public void returned(){
+    currentHolder=null;
+    dueDate=null;
+  }
+  public String circulationStatus(
+    if (currentHolder != null){
+      return currentHolder+ " " + dueDate;
+    }
+      return "book available on shelves" ;
+    }
+    
 }
