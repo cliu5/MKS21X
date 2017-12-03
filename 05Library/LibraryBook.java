2d4125de56a983365149c1e0710a7a3b3c8ae5e9
cliu5 <cliu5@stuy.edu>
2017-12-03 16:02:14 -0500

    Update LibraryBook.java

--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -1,17 +1,24 @@
 abstract Class LibraryBook extends Book implements Comparable<LibraryBook>{
     private String callNumber;
-    public LibraryBook(aut,t,is,cal){
-	Author=aut;
-	Title=t;
-	ISBN=is;
-	callNumber=cal;
+    public LibraryBook(String _author, String _title, String _ISBN, String _callNumber){
+	author=_author;
+	title=_title;
+	ISBN=_ISBN;
+	callNumber=_callNumber;
     }
     public string getCallNumber(){
 	return callNumber;
     }
-    public abstract void checkout(String patron,String due){
+    public abstract void checkout(String patron,String _dueDate){
     }
     public abstract void returned(){
     }
     public abstract String circulationStatus(){
     }
+    public int compareTo(LibraryBook other){
+	return callNumber.compareTo(other.getCallNumber());
+    }
+    public String toString(){
+	return super.toString() + " " + circulationStatus() + " " + callNumber;
+    }
+}
