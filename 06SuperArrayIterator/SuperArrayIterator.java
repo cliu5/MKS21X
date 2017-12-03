0d8f2f8710109183de0efcd224395919939019fb
cliu5 <cliu5@stuy.edu>
2017-12-03 16:57:07 -0500

    Update SuperArrayIterator.java

--- 06SuperArrayIterator/SuperArrayIterator.java
+++ 06SuperArrayIterator/SuperArrayIterator.java
@@ -1,1 +1,27 @@
 import java.util.*
+public class SuperArrayIterator implements Iterator<String>{
+private String[] data;
+private int current;
+private int end;
+public SuperArrayIterator (String[]data, int size){
+  this.data=data;
+  current=0;
+  end=size-1;
+  }
+ public void remove(){
+  throw new UnsupportedOperationException();
+  }
+  
+ public boolean hasNext(){
+ return current<=end;
+ }
+ public String next(){
+ if (hasNext()){
+ current++;
+ }
+ else{
+ System.exit(0);
+ }
+ return data[current-1];
+ }
+ }
