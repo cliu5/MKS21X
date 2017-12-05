89c2a04cce725ad5d6c54f5ed3982750a4abb73e
cliu5 <cliu5@stuy.edu>
2017-12-04 21:48:48 -0500

    Update Barcode.java

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,3 +1,10 @@
 public class Barcode {
+  private String zip;
+  public Barcode(String _zip){
+    if (zip.length!=5|| (text.matches("^[0-9]*$"))) {
+    zip=_zip;
+    }
+    throw new IllegalArgumentException;
 
+}
 }
