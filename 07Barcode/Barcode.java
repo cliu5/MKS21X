775d2651c535748a8d06c6e6f5f6ca2d85ff88fb
cliu5 <cliu5@stuy.edu>
2017-12-04 22:04:28 -0500

    Update Barcode.java

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,5 +1,16 @@
 public class Barcode {
   private String zip;
+private match = new String[10];
+	match[1]=  ":::||";
+match[2]=  "::|:|";
+match[3]=  "::||:";
+match[4]=  ":|::|";
+match[5]=  ":|:|:";
+match[6]=  ":||::";
+match[7]=  "|:::|";
+match[8]=  "|::|:";
+match[9]=  "|:|::";
+match[0]=  "||:::";  
   public Barcode(String _zip){
     if (zip.length()!=5|| (zip.matches("^[0-9]*$"))) {
     zip=_zip;
