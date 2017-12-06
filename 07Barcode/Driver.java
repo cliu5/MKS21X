ef082ccc505ddb0e00332ad9728eb40a937284f1
cliu5 <cliu5@stuy.edu>
2017-12-05 23:29:21 -0500

    Update Driver.java

--- 07Barcode/Driver.java
+++ 07Barcode/Driver.java
@@ -1,11 +1,11 @@
 public class Driver{ 
 public static void main(String[] args){
 	Barcode e = new Barcode("asdfd"); //Contains non-barcode characters, should throw IllegalArgumentException
-	//	Barcode f = new Barcode("1234"); //Invalid length, should throw IllegalArgumentException
+		Barcode f = new Barcode("1234"); //Invalid length, should throw IllegalArgumentException
 
-	//      System.out.println(Barcode.toCode("00294")); // |||:::||:::::|:||:|:::|::|:|:|:|
-	//	System.out.println(Barcode.toCode("asdfd")); //Contains non-barcode characters, should throw IllegalArgumentException
-	//	System.out.println(Barcode.toCode("1234")); //Invalid length, should throw IllegalArgumentException
+	      System.out.println(Barcode.toCode("00294")); // |||:::||:::::|:||:|:::|::|:|:|:|
+		System.out.println(Barcode.toCode("asdfd")); //Contains non-barcode characters, should throw IllegalArgumentException
+		System.out.println(Barcode.toCode("1234")); //Invalid length, should throw IllegalArgumentException
 
 	//	System.out.println(Barcode.toZip("|||:::||:::::|:||:|:::|::|:|:|:|")); //Should return 00294
 	//	System.out.println(Barcode.toZip("|:::||:::||::|:|:|::|:|:|:::||:")); //Invalid length, should throw IllegalArgumentException
