59609c7d0a986c882d66894b6ef81cf96b58de68
cliu5 <cliu5@stuy.edu>
2017-12-13 14:39:01 -0500

    Update TemperatureWindow.java

--- 09GUI/TemperatureWindow.java
+++ 09GUI/TemperatureWindow.java
@@ -6,6 +6,7 @@ public class Window2 extends JFrame {
 
  private JButton b;
  private JTextField t;
+  private JCheckBox c;
  
   //CONSTRUCTOR SETS EVERYTHING UP
   public Window2() {
@@ -18,14 +19,25 @@ public class Window2 extends JFrame {
     
      b = new JButton("Convert!");
      t = new JTextField(12);
+     c = new JCheckBox("Farenheit?")
      pane.add(b);
      pane.add(t);
+   pane.add(c);
  }
  public void actionPerformed(ActionEvent e){
  String event=e.getActionCommand();
  if(event.equals("Convert!"){
-    t.setText();
+  if(c.isSelected()){
+   t.setText(FtoC(Double.parseDouble(t.getText)));
+  }
     }
+  
+public static double CtoF(double t){
+	return 9 * t / 5 + 32;
+    }
+public static double FtoC(double t){
+	return 5 * (t - 32) / 9;
+}
 
   //MAIN JUST INSTANTIATES + MAKE VISIBLE
   public static void main(String[] args) {
