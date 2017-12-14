f6fb4a3189d1b8b41e07360f762e692f07de13e6
cliu5 <cliu5@stuy.edu>
2017-12-13 20:44:20 -0500

    Update TemperatureWindow.java

--- 09GUI/TemperatureWindow.java
+++ 09GUI/TemperatureWindow.java
@@ -29,20 +29,28 @@ public class TemperatureWindow extends JFrame implements ActionListener{
 		
   //CONSTRUCTOR SETS EVERYTHING UP
   public TemperatureWindow() {
-     this.setSize(600,400);
-     this.setLocation(100,100);
-     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    
-     pane = this.getContentPane();
-     pane.setLayout(new FlowLayout());  //NOTE!! THIS CAN BE CHANGED (see below)
-    
-     b = new JButton("Convert!");
-     t = new JTextField(12);
-     c = new JCheckBox("Farenheit?");
-     pane.add(b);
-     pane.add(t);
-   pane.add(c);
- }
+        this.setTitle("My first GUI");
+        this.setSize(500,80);
+        this.setLocation(100,100);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        pane = this.getContentPane();
+        pane.setLayout(new FlowLayout());
+        b = new JButton("Convert!");
+        c = new JCheckBox("Farenheit?);
+        t = new JTextField(15);
+
+        b.addActionListener(this);
+        b2.addActionListener(this);
+        t.addActionListener(this);
+        c.addActionListener(this);
+
+        pane.add(t);
+        pane.add(c);
+        pane.add(b);
+        pane.add(b2);
+
+    }
  
 public static double CtoF(double t){
 	return 9 * t / 5 + 32;
