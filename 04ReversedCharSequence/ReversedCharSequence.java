c40c86cb139303f160ad66a526ba5b28cbf8261b
cliu5 <cliu5@stuy.edu>
2018-01-03 21:36:12 -0500

    Update ReversedCharSequence.java

--- 04ReversedCharSequence/ReversedCharSequence.java
+++ 04ReversedCharSequence/ReversedCharSequence.java
@@ -7,14 +7,23 @@ public class ReversedCharSequence implements CharSequence{
 	    stuff+=x.substring(i,i+1);
 	}    
     }
+	//jack at the dojo told me to put this in//
+	  public ReversedCharSequence (ReversedCharSequence x) {
+    this(x.toString());
+  }
     public char charAt(int index){
 	return stuff.charAt(index);
     }
     public int length(){
 	return stuff.length();
     }
-    public CharSequence subSequence(int start, int end){
-	return stuff.substring( start , end);
+public String toString(){
+	return stuff;
+}
+    public ReversedCharSequence subSequence(int start, int end){
+	    String temp=stuff.substring(start,end);
+	    ReversedCharSequence ans=new ReversedCharSequence(temp);
+	    return new ReversedCharSequence(ans);
     }
 
 
