aa92ed392d68bd59f34a8dfb0251445a2670eca5
cliu5 <cliu5@stuy.edu>
2018-01-03 23:45:00 -0500

    Update ReversedCharSequence.java

--- 04ReversedCharSequence/ReversedCharSequence.java
+++ 04ReversedCharSequence/ReversedCharSequence.java
@@ -27,19 +27,6 @@ public String toString(){
     }
 
 
-	 public static void main(String[]args){
-    ReversedCharSequence a = new ReversedCharSequence("Hello");
-    ReversedCharSequence b = new ReversedCharSequence(a);
-    
-    System.out.println(a);//olleH
-    System.out.println(b);//Hello
-    System.out.println(a.charAt(3));//e
-    System.out.println(a.length());//5
-    System.out.println(a.toString());//olleH
-    System.out.println(a.subSequence(2,4));//le
-    System.out.println(a.subSequence(0,3));//oll
-    System.out.println(a);//olleH
-  }
 	
 	
 	
